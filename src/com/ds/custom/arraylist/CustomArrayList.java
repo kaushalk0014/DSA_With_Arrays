a7513507fbd52d1421a26eb9ab40e4be21f706a7
kaushal kumar <kaushal.k.0014@gmail.com>
2025-05-26 20:59:09 +0530

    adding custom array list method

---
+++ src/com/ds/custom/arraylist/CustomArrayList.java
@@ -0,0 +1,28 @@
+package com.ds.custom.arraylist;
+
+public class CustomArrayList {
+
+	public boolean add(int value) {
+		return true;
+	}
+
+	public boolean contains(int value) {
+		return true;
+	}
+	
+	public boolean remove(int value) {
+		return true;
+	}
+	
+	public boolean addAll(int[] values) {
+		return true;
+	}
+	
+	public int get(int index) {
+		return 0;
+	}
+	
+	public int size() {
+		return 0;
+	}
+}
---
+++ src/com/ds/custom/arraylist/CustomArrayListTest.java
@@ -0,0 +1,8 @@
+package com.ds.custom.arraylist;
+
+public class CustomArrayListTest {
+
+	public static void main(String[] args) {
+		
+	}
+}
